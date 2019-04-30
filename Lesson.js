8068e7835f5bf54663adf3e5ef5e1ab8f3717843
Svetlana <andreevas1989@gmail.com>
2019-04-30 16:06:27 +0600

    Second

---
+++ Lesson.js
@@ -0,0 +1,54 @@
+'use strict'
+
+var number = 5;
+var string = "HEllo world";
+var sym = Symbol();
+
+var boolean = true;
+
+
+
+let persone ={
+    name: "Jone", 
+    age: 25,
+    isMarried: false
+}
+
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+//console.log(arr[0]);
+
+/*alert("Hello wotld");
+let answer = confirm("Who are you?");
+console.log(answer);*/
+
+//let answer = +prompt ("Есть ди вам 18 лет?", "Yes");
+
+
+//console.log(typeof(answer));
+//console.log("arr" + "- object");
+//console.log(4 + +"4");
+
+let incr = 10,
+    decr = 10;
+
+   
+   // console.log(incr++);
+  //  console.log(decr--);
+
+ //   console.log("5" == "5");
+
+let isChecked = true,
+    isClose = false;
+
+  //  console.log(!isChecked || isClose);
+
+
+console.log(isChecked && !isClose);
+
+
+
+
+
+
